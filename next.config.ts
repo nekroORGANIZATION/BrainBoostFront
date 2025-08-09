8dc0f9b8e9d2e3cfaa02bbb9c374cac74ca0a21e
nekroMAYS <106478287+NaviOldManTop@users.noreply.github.com>
2025-08-09 12:44:00 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,         // включаем поддержку App Router
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;

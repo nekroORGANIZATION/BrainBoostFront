46114e1a920af0d7f06278100443bf5164c6afa7
nekroMAYS <106478287+NaviOldManTop@users.noreply.github.com>
2025-09-25 22:55:43 +0300

    Delete src/app/teacher/lessons directory

--- src/app/teacher/lessons/layout.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-export default function SectionLayout({ children }: { children: React.ReactNode }) {
-  return <React.Suspense fallback={null}>{children}</React.Suspense>;
-}

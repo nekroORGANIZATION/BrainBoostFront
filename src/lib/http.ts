c83dc09f10f70ae76c9744ab2d72540041b3c96f
nekroMAYS <106478287+NaviOldManTop@users.noreply.github.com>
2025-09-26 00:14:10 +0300

    Update http.ts

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,8 +1,7 @@
 // src/lib/http.ts
 import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 
-export const API_BASE =
-  process.env.NEXT_PUBLIC_API_BASE || 'https://brainboost.pp.ua/api';
+export const API_BASE = 'https://brainboost.pp.ua/api';
 
 // ← звір ці шляхи з бекендом і поправ, якщо інші:
 export const LOGIN_URL    = '/accounts/api/login/';
